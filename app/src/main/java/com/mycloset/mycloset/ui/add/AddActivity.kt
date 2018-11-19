0be98b0ea382172b2dbfe3fa4a0736a372ff5c48
sovovy <sovovy@gmail.com>
2018-11-20 04:25:37 +0900

    날씨 이미지 추가. <오늘의 기록> EditText 속성 변경. <오늘 탭> -> <오늘의 기록> 연결.

--- app/src/main/java/com/mycloset/mycloset/ui/add/AddActivity.kt
+++ app/src/main/java/com/mycloset/mycloset/ui/add/AddActivity.kt
@@ -1,13 +1,13 @@
-package com.mycloset.mycloset.ui.today
+package com.mycloset.mycloset.ui.add
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.mycloset.mycloset.R
 
-class TodayActivity : AppCompatActivity() {
+class AddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add)
     }
 }
--- app/src/main/java/com/mycloset/mycloset/ui/today/TodayFragment.kt
+++ app/src/main/java/com/mycloset/mycloset/ui/today/TodayFragment.kt
@@ -1,15 +1,21 @@
 package com.mycloset.mycloset.ui.today
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mycloset.mycloset.R
+import com.mycloset.mycloset.ui.add.AddActivity
+import kotlinx.android.synthetic.main.fragment_today.view.*
 
 class TodayFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_today,container,false)
+        v.today_add_ib.setOnClickListener {
+            startActivity(Intent(activity, AddActivity::class.java))
+        }
         return v
     }
 }
