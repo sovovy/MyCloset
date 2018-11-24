56f86737aac00a7c7fc68b7f57a3c0100a9574d2
sovovy <sovovy@gmail.com>
2018-11-25 00:26:01 +0900

    아이콘 이미지 추가

--- app/src/main/java/com/mycloset/mycloset/ui/theday/ThedayActivity.kt
+++ app/src/main/java/com/mycloset/mycloset/ui/theday/ThedayActivity.kt
@@ -2,6 +2,7 @@ package com.mycloset.mycloset.ui.theday
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.mycloset.mycloset.R
 
 class ThedayActivity : AppCompatActivity() {
 
