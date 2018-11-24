7ae538985fe0ba414c7df3c78afe52f04c99705e
sovovy <sovovy@gmail.com>
2018-11-25 00:15:55 +0900

    <그날의 기록> 보는 activity, 수정하는 activity xml 완성.

---
+++ app/src/main/java/com/mycloset/mycloset/ui/edit/EditActivity.kt
@@ -0,0 +1,12 @@
+package com.mycloset.mycloset.ui.edit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+}
---
+++ app/src/main/java/com/mycloset/mycloset/ui/theday/ThedayActivity.kt
@@ -0,0 +1,12 @@
+package com.mycloset.mycloset.ui.theday
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ThedayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theday)
+    }
+}
