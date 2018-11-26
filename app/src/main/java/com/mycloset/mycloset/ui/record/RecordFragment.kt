8823205ec56fc5fd1742db97dbdbbd102c8dbb20
johnyejin <yejin7304@gmail.com>
2018-11-27 07:18:58 +0900

    *recyclerview랑 cardview 연결 *realmdb plugin 추가 *fragment_record, item_card xml 수정

--- app/src/main/java/com/mycloset/mycloset/ui/record/RecordFragment.kt
+++ app/src/main/java/com/mycloset/mycloset/ui/record/RecordFragment.kt
@@ -2,14 +2,60 @@ package com.mycloset.mycloset.ui.record
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mycloset.mycloset.R
+import io.realm.Realm
+import kotlinx.android.synthetic.main.fragment_record.*
+import kotlinx.android.synthetic.main.fragment_record.view.*
 
 class RecordFragment : Fragment() {
+
+    lateinit var recordRealm: Realm
+    lateinit var recordAdapter: RecordAdapter
+    lateinit var recordItems: ArrayList<RecordItem>
+    
+
+
+    fun init() {
+        Realm.init(this.context)    // ?
+        recordRealm = Realm.getDefaultInstance()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_record,container,false)
+        //Log.d("asd",recordItems.size.toString())
+
+        Realm.init(this.context)   // this.context 아님
+
+        recordItems = ArrayList()
+        recordItems.add(RecordItem(0,"2018-12-22",3,"sunny",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"cloud",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"cloud2",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"lighting",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"rain",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"snow",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"sunny",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"sunny",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"sunny",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"sunny",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"sunny",19.4f,21.4f,"g","gg","g","ggg"))
+        recordItems.add(RecordItem(0,"2018-12-22",3,"sunny",19.4f,21.4f,"g","gg","g","ggg"))
+
+
+
+
+        recordAdapter = RecordAdapter(recordItems)
+        val glm = GridLayoutManager(context,2, GridLayoutManager.HORIZONTAL, false)
+        glm.orientation = RecyclerView.VERTICAL
+        v.record_rv.layoutManager = glm
+        v.record_rv.adapter = recordAdapter
+
+
         return v
     }
 }
---
+++ app/src/main/java/com/mycloset/mycloset/ui/record/RecordViewHolder.kt
@@ -0,0 +1,27 @@
+package com.mycloset.mycloset.ui.record
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.mycloset.mycloset.R
+
+class RecordViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    var recordDate : TextView = itemView!!.findViewById(R.id.item_tv_date) as TextView              // 오늘 날짜
+
+    var recordWeather : ImageView = itemView!!.findViewById(R.id.item_iv_weather) as ImageView      // 오늘 날씨 아이콘
+    var recordWeatherBackground : ImageView = itemView!!.findViewById(R.id.item_iv_background) as ImageView  // 오늘 날씨 배경
+
+    var recordTemper : TextView = itemView!!.findViewById(R.id.item_tv_temper) as TextView          // 기온
+
+    var recordFeel : TextView = itemView!!.findViewById(R.id.item_tv_feel) as TextView              // 체감 온도
+
+    var recordOuter : TextView = itemView!!.findViewById(R.id.item_tv_outer) as TextView            // 겉옷
+
+    var recordTop : TextView = itemView!!.findViewById(R.id.item_tv_top) as TextView                // 상의
+
+    var recordBottom : TextView = itemView!!.findViewById(R.id.item_tv_bottom) as TextView          // 하의
+
+    var recordMemo : TextView = itemView!!.findViewById(R.id.item_tv_memo) as TextView              // 메모
+
+}
