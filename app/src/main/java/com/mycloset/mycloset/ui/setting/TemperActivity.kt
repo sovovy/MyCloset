c62dafff0f439b44b57858c4b837f0fed1bc2952
JIN0419 <0419kj@naver.com>
2018-12-11 02:55:02 +0900

    <위치 설정> 글씨체, 간격 수정. <온도 오차 설정> 구현. <문의> 구현

---
+++ app/src/main/java/com/mycloset/mycloset/ui/setting/TemperActivity.kt
@@ -0,0 +1,12 @@
+package com.mycloset.mycloset.ui.setting
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TemperActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temper)
+    }
+}
