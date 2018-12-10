8f6f773a5081cbaefbb03b66d24b20a98ca94d04
sovovy <sovovy@gmail.com>
2018-12-11 08:36:43 +0900

    이스터에그

--- app/src/main/java/com/mycloset/mycloset/ui/setting/QuestionActivity.kt
+++ app/src/main/java/com/mycloset/mycloset/ui/setting/QuestionActivity.kt
@@ -2,12 +2,20 @@ package com.mycloset.mycloset.ui.setting
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.mycloset.mycloset.R
+import kotlinx.android.synthetic.main.activity_question.*
 
 class QuestionActivity : AppCompatActivity() {
+    var cnt = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+        question_ayoungi.setOnClickListener {
+            cnt++
+            if(cnt==10)
+                dvlp.visibility = View.VISIBLE
+        }
     }
 }
